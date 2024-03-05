8bc1686a7d4f721d51c6f0394cdc9f98606e0367
OprescuSebastian <sebi.oprescu@yahoo.com>
2024-03-05 13:50:56 +0200

    First Homework commit and pushed

---
+++ session4/src/com/itschool/course/Course4Main.java
@@ -0,0 +1,13 @@
+package com.itschool.course;
+
+public class Course4Main {
+
+    public static void main(String[] args) {
+        String firstName = "Sebastian";
+        String lastName = "Oprescu";
+        System.out.println(firstName + lastName);
+
+        firstName += lastName;
+        System.out.println(lastName);
+    }
+}
