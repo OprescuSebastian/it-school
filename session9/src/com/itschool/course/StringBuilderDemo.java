49ef29433f86516cc858bf6ee29a085c47b84238
OprescuSebastian <sebi.oprescu@yahoo.com>
2024-03-26 14:39:30 +0200

    main commit

---
+++ session9/src/com/itschool/course/StringBuilderDemo.java
@@ -0,0 +1,11 @@
+package com.itschool.course;
+
+public class StringBuilderDemo {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("text");
+        sb.append("ionutz");
+
+        System.out.println(sb);
+    }
+}
