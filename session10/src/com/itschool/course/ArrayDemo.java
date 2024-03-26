c3921c99e78c5ba141fdd1bbff337b2b4d674f84
OprescuSebastian <sebi.oprescu@yahoo.com>
2024-03-26 14:38:45 +0200

    main commit

---
+++ session10/src/com/itschool/course/ArrayDemo.java
@@ -0,0 +1,14 @@
+package com.itschool.course;
+
+import java.util.Arrays;
+
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+        int[] inArray = {1, 2, 3};
+        int[] zeroArray = new int[4];
+
+        System.out.println(Arrays.toString(zeroArray));
+
+    }
+}
--- session4/src/com/itschool/course/CursMain4.java
+++ session4/src/com/itschool/course/CursMain4.java
@@ -1,7 +1,37 @@
 package com.itschool.course;
 
 public class CursMain4 {
-    public static void main(String[] args) {
-        System.out.println("hello World" );
+
+
+    public static void main(String... args) {
+
+        /*System.out.println("Hello World");
+        String firstName = "Ionutz";
+        String lastName = "Baur";
+
+        System.out.println(firstName + lastName);
+
+        firstName += lastName;
+        System.out.println(firstName);
+
+        int number = -10;
+
+        boolean trueVar = true;
+        System.out.println("trueVar is:" + trueVar);
+
+        System.out.println("negation of trueVar:" + !trueVar);
+
+        boolean falseVar = false;
+        System.out.println("falseVar is:" + falseVar);
+
+        System.out.println("negation of falseVar" + !falseVar);*/
+
+        charDemo();
+    }
+
+    static void charDemo() {
+        char a = 'x';
+        int b = 2;
+        System.out.println(a + b);
     }
 }
