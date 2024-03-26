37ac0e28de4298e08892f8a66ccab4c1a8942bf4
OprescuSebastian <sebi.oprescu@yahoo.com>
2024-03-26 14:55:04 +0200

    remade homework

---
+++ session6/src/com/itschool/homeworks6/HomeworkS6Ex3.java
@@ -0,0 +1,16 @@
+package com.itschool.homeworks6;
+
+public class HomeworkS6Ex3 {
+
+    public static void main(String[] args) {
+
+
+        int age = 20;
+        if (age > 18) {
+            System.out.println("I am adult");
+        } else {
+            System.out.println("i am not adult");
+        }
+
+    }
+}
