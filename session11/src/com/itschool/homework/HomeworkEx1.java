6c58f7614153f03fafbfbcc3c8c118ce903a41af
OprescuSebastian <sebi.oprescu@yahoo.com>
2024-03-31 14:22:18 +0300

    Homework session11

---
+++ session11/src/com/itschool/homework/HomeworkEx1.java
@@ -0,0 +1,11 @@
+package com.itschool.homework;
+
+public class HomeworkEx1 {
+
+    public static void main(String[] args) {
+        Techers George = new Techers("Grigrescu", "George", "30/02/1978", 12351321451234L, "Male", "Targoviste Bl.Unirii B4 Ap.3", 32);
+        Students Ion = new Students("Ivascu" , "Ion" , "21/04/2003",1495731642715L , "male" , "Bucuresti Bl.Unirii B2 Ap.3 ", 21  );
+        Students Alina = new Students("Diaconescu", "Alina","01/04/2004",2416124124123L, "female","Targoviste Str.Stefan Cel Mare nr.194", 20);
+
+    }
+}
